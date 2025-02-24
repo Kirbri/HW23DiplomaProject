6bc154b98ad5ec663b38bbefae3d1200650ff2b6
Ann Kornilova <Kirbri@yandex.ru>
2025-02-24 23:33:55 +0300

    Initialization commit

---
+++ src/test/java/avito/pages/ItemPage.java
@@ -0,0 +1,24 @@
+package avito.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ItemPage {
+    private final SelenideElement titleInfoText = $("[data-marker='item-view/title-info']"),
+            favoriteSellerSubscriptionButton = $("[data-marker='favorite-seller-subscription-button']");
+
+    @Step("Проверить соответствие названий '{value}' объявлений")
+    public ItemPage checkTitleAnnouncement(String value) {
+        titleInfoText.shouldHave(text(value));
+        return this;
+    }
+
+    @Step("Подписаться на продавца")
+    public ItemPage subscribeFavoriteSeller() {
+        favoriteSellerSubscriptionButton.click();
+        return this;
+    }
+}
---
+++ src/test/java/config/WebDriverConfig.java
@@ -0,0 +1,36 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:properties/${environment}.properties",
+        "classpath:properties/local.properties",
+})
+
+public interface WebDriverConfig extends Config {
+
+    @Key("browser.name")
+    @DefaultValue("chrome")
+    String getBrowser();
+
+    @Key("browser.version")
+    String getBrowserVersion();
+
+    @Key("browser.windowSize")
+    @DefaultValue("1920x1080")
+    String getBrowserWindowSize();
+
+    @Key("browser.isRemote")
+    @DefaultValue("false")
+    boolean isRemote();
+
+    @Key("remote.user")
+    String getRemoteUser();
+
+    @Key("remote.password")
+    String getRemotePassword();
+
+    @Key("remote.url")
+    @DefaultValue("selenoid.autotests.cloud")
+    String getRemoteUrl();
+}
